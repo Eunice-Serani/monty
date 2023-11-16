c2cc76ad3caefb3f4d841b267f147799d79cc451
Alfred Nti <87778148+Freddywhest@users.noreply.github.com>
2023-11-16 09:16:47 +0000

    Updated monty.h and deleted monty

--- monty.h
+++ monty.h
@@ -10,51 +10,52 @@
 #include <ctype.h>
 
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
+ * struct stack_s - Doubly linked list representation of a stack (or queue)
+ * @n: Integer value
+ * @prev: Points to the previous element of the stack (or queue)
+ * @next: Points to the next element of the stack (or queue)
  *
- * Description: doubly linked list node structure
+ * Description: Doubly linked list node structure
  * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
 } stack_t;
 
 /**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
+ * struct instruction_s - Opcode and its function
+ * @opcode: The opcode
+ * @f: Function to handle the opcode
  *
- * Description: opcode and its function
+ * Description: Opcode and its function
  * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+    char *opcode;
+    void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /**
- * struct bus_s - variables -args, file, line content
- * @arg: value
- * @file: pointer to monty file
- * @content: line content
- * @lifi: flag change stack <-> queue
+ * struct bus_s - Variables - args, file, line content
+ * @arg: Value
+ * @file: Pointer to monty file
+ * @content: Line content
+ * @lifi: Flag change stack <-> queue
  *
- * Description: carries values through the program
+ * Description: Carries values through the program
  */
 typedef struct bus_s
 {
-	char *arg;
-	FILE *file;
-	char *content;
-	int lifi;
-}  bus_t;
+    char *arg;
+    FILE *file;
+    char *content;
+    int lifi;
+} bus_t;
+
 extern bus_t bus;
 
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
@@ -62,11 +63,14 @@ ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
 ssize_t getstdin(char **lineptr, int file);
 
-char  *clean_line(char *content);
+char *clean_line(char *content);
 void free_stack(stack_t *head);
 
 int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
 
+/**
+ * Opcode functions
+ */
 void f_push(stack_t **head, unsigned int number);
 void f_pall(stack_t **head, unsigned int number);
 void f_pint(stack_t **head, unsigned int number);
@@ -86,6 +90,9 @@ void f_pstr(stack_t **head, unsigned int counter);
 void f_rotl(stack_t **head, unsigned int counter);
 void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
 
+/**
+ * Stack and queue functions
+ */
 void addnode(stack_t **head, int n);
 void addqueue(stack_t **head, int n);
 void f_queue(stack_t **head, unsigned int counter);
